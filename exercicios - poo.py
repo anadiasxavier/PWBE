651e1f342e184e7628ac682c5449d32c499dff6f
Ana Clara Dias Xavier <54588459864@>
2025-02-17 09:42:11 -0300

    .

--- exercicios - poo.py
+++ exercicios - poo.py
@@ -238,12 +238,12 @@ Paciente = paciente(nome, idade, consulta)
 Paciente.historicoConsulta()'''
 
 #EXERCICIO 10 
-class livro:
+'''class livro:
         def __init__(self, titulo, autor , numPagina):
             self.titulo = titulo
             self.autor = autor
             self.numPagina = numPagina
-            self.estoque = 0
+            self.estoque = 1
 
         def emprestarLivro(self):
             opcao = int(input("Deseja emprestar um livro? \n1-Sim \n2-Não "))
@@ -284,4 +284,231 @@ numPagina = int (input("Digite o número de páginas do livro: "))
 livro = livro(titulo, autor, numPagina)
 livro.emprestarLivro()
 livro.devolverLivro()
-livro.verificar()
+livro.verificar()'''
+
+#EXERCICIO 11
+
+'''class Banco:
+    def __init__(self):
+        self.saldo = 0 
+        self.nome = ""
+        self.idade = 0
+        self.cpf = 0
+        self.nomeResponsavel = ""
+        self.numConta = 0
+        self.senha = 0
+        self.rg = 0
+        self.cep = 0
+    
+    def cadastrar(self):
+        self.nome = str (input("Digite o seu nome: "))
+        self.idade = int (input("Digite a sua idade: "))
+        self.cpf = int(input("Digite o seu CPF: "))
+        self.nomeResponsavel = str(input("Digite o nome do seu responsavel: "))
+        print("Cadastro concluido!")
+
+    def abrirConta(self):
+        self.numConta = int(input("Digite o número da conta: "))
+        self.senha = int(input("Digite a sua senha: "))
+        self.rg = int(input("Digite o número de rg: "))
+        self.cep = int(input("Digite o seu CEP: "))
+        self.saldo = 0
+        print("Sua conta foi aberta com sucesso!")
+
+    def movimentacao(self):
+        opcao = int(input("1-saque \n2-depositar \n3-transferência \nO que deseja fazer? "))
+
+        if opcao == 1:
+            print(f"O seu saldo é {self.saldo}")
+            valor = float(input("Quanto deseja sacar? "))
+            if valor <= self.saldo:
+                self.saldo -= valor
+                print("Valor sacado com sucesso!")
+            else:
+                print("O seu saldo é insuficiente para completar esse saque.")
+
+        elif opcao == 2:
+            print(f"O seu saldo é {self.saldo}")
+            valor = float(input("Quanto deseja depositar? "))
+            print("Valor depositado com sucesso!")
+            self.saldo += valor
+        
+        elif opcao == 3:
+            nomePessoa = str(input("Digite o nome do usuario da conta que deseja tranferir: "))
+            contaPessoa = int(input("Digite o número da conta: "))
+            valor = float(input("Quanto deseja transferir?"))
+
+            if valor <= self.saldo:
+                self.saldo -= valor
+                print(f"Você realizou uma transferência para {nomePessoa} no valor de {valor}.")
+            else:
+                print("Saldo insuficiente!")
+
+banco = Banco() 
+banco.cadastrar()
+banco.abrirConta()
+banco.movimentacao()'''
+
+#EXERCICIO 12
+
+'''class LojaVirtual:
+    def __init__(self):
+        self.listaProdutos = [
+            {'Tipo de produto': 'Blusa', 'preco': 200, 'id': 10},
+            {'Tipo de produto': 'Calça', 'preco': 350, 'id': 15},
+            {'Tipo de produto': 'Short', 'preco': 150, 'id': 20} 
+        ]
+   
+
+    def cadastro(self):
+        nomeProduto = str(input("Digite o nome do produto: "))
+        precoProduto = float(input("Digite o preço do produto: "))
+        IdProduto = int(input("Digite o id do produto: "))
+        produto={
+            'nome': nomeProduto,
+            'preco': precoProduto,
+            'id': IdProduto
+        }
+        self.listaProdutos.append(produto)
+        print("Produto cadastrado com sucesso!")
+
+    def escolherProduto(self):
+        print(self.listaProdutos)
+        print("Esses são os nossos produtos disponiveis!")
+        opcao = int (input("1-Sim 2-Não \nDeseja comprar algo?"))
+            
+            
+        if opcao == 1:
+            self.produtoEscolhido = int(input("Digite o id do produto que deseja: "))
+            opcao2 = int(input("Deseja mais algum produto? \n1- sim\n2-não\nDigite a opção escolhida: "))
+            if opcao2 == 1:
+                self.escolherProduto()
+            elif opcao2 ==2:
+                self.Carrinho()
+        else:
+            print("Volte sempre!")
+
+    def Carrinho(self):
+      
+        for produto in self.listaProdutos:
+            if produto ['id'] == self.produtoEscolhido:
+                quantproduto = int(input("Digite quantas vezes deseja comprar esse mesmo produto: "))
+                valorTotal = self.produtoEscolhido['preco'] * quantproduto #Pedi ajuda para o professor!!!!!!!!!
+                print(f"O valor da compra sem o desconto é R${valorTotal}") 
+                valorFinal = valorTotal - 20
+                print(f"O valor final com o desconto é R${valorFinal}")
+                print("Compra realizada com sucesso!")
+                return
+        
+            print("Produto não encontrado. Verifique o ID e tente novamente.")
+
+loja = LojaVirtual()
+loja.escolherProduto()'''
+
+#EXERCICIO 13
+'''
+class Agenda:
+    def __init__(self):
+        self.listaTefonica = [
+            {'Nome': 'Ana', 'numero': 4002892},
+            {'Nome': 'Xavi', 'numero': 101010}
+        ]
+    
+    def adicionar(self):
+        print(self.listaTefonica)
+        nomeTelefone = str(input("Digite o nome do numero do telefone que deseja adicionar: "))
+        numeroTelefone = int(input("Digite o numero que deseja adicionar: "))
+        contatos = {
+            'Nome':nomeTelefone,
+            'telefone': numeroTelefone
+        }
+        self.listaTefonica.append(contatos)
+        print("Contato adicionado com sucesso!")
+        print(self.listaTefonica)
+    
+        self.menu()
+    
+    def remover(self):
+        print(self.listaTefonica)
+        nomeRemove = str(input("Qual nome deseja remover?"))
+        for contato in self.listaTefonica:
+            if contato['Nome'] == nomeRemove:
+                self.listaTefonica.remove(contato)
+                print("O contato foi removido!")
+            else:
+                print("Esse contato não existe")
+        print(self.listaTefonica)
+        self.menu()
+
+    def buscar(self):
+        telefoneBusca = int(input("Digite o número que está procurando: "))
+        for contato in self.listaTefonica:
+            if contato ['numero'] ==  telefoneBusca:
+                print(f"O número foi encontrado na lista! O número {telefoneBusca} pertence a {contato['Nome']}")
+                self.menu()
+                return
+        print("Não foi encotrado esse número!")
+        print(self.listaTefonica)
+        self.menu()
+    
+    def menu(self):
+        print("Bem-vindo a agenda da Ana")
+        opcao = int (input("O que você deseja: \n1-Adicionar \n2-Remover \n3-Buscar"))
+
+        if opcao == 1:
+            self.adicionar()
+        elif opcao == 2:
+            self.remover()
+        elif opcao  == 3:
+            self.buscar()
+        else:
+            print("Opção inválida!")
+    
+agenda = Agenda()
+agenda.menu()'''
+
+#EXERCICIO 14
+
+class MaquinaVendas:
+    def __init__(self):
+        self.listaProdutos[
+            {'Nome': 'Celular' , 'Valor': 1000 , 'Estoque': 10},
+            {'Nome': 'Fone de ouvido', 'Valor': 200, 'Estoque': 8}
+        ]
+
+    
+    def cadastrar(self):
+        print(self.listaProdutos)
+        nomeProduto = str (input("Digite o nome do produto que deseja cadastrar: "))
+        precoProduto = float("Digite o preço do novo produto: ")
+        self.estoqueProduto = int(input("Digite o a quantidade de estoque desse produto: "))
+
+        produtos = {
+            'Nome': nomeProduto,
+            'Valor': precoProduto,
+            'Estoque': self.estoqueProduto
+        }
+        self.listaProdutos.append(produtos)
+        print("Produto adicionado com sucesso!")
+
+    def selecionarProduto(self):
+        self.listaSelecionados[]
+        print(self.listaprodutos)
+        produtoSelecionado = str(input("Digite o nome do produto que deseja selecionar: "))
+        self.quantProdutos = int(input("Digite a quantidade de produtos desejada: "))
+        for produtos in self.listaprodutos:
+            if produtos ['Nome'] == produtoSelecionado:
+                self.listaSelecionados.append(produtoSelecionado)
+    
+    def caixa(self):
+        print("A sua lista de produtos que dejesa comprar é: ")
+        print(self.listaSelecionados)
+
+        valorpago = float(input("Insira o valor do dinheiro para efetuar o pagamento: "))
+        valorTotal = self.listaSelecionados['Preco']
+        valorTroco = valorpago - valorTroco
+        estoqueNovo = self.estoqueProduto - self.quantprodutos
+
+        if 
+
+
